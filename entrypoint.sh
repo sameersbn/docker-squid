8204dbd8f1eec5dee836e6f49882c968ee480883
Sameer Naik <sameer@damagehead.com>
2015-08-09 22:43:53 +0530

    create functions for better maintenence and readability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 set -e
 
-# fix permissions on the log dir
-mkdir -p ${SQUID_LOG_DIR}
-chmod -R 755 ${SQUID_LOG_DIR}
-chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG_DIR}
+create_log_dir() {
+  mkdir -p ${SQUID_LOG_DIR}
+  chmod -R 755 ${SQUID_LOG_DIR}
+  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG_DIR}
+}
 
-# fix permissions on the cache dir
-mkdir -p ${SQUID_CACHE_DIR}
-chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
+create_cache_dir() {
+  mkdir -p ${SQUID_CACHE_DIR}
+  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
+}
 
-# deprecated: backward compatibility
-if [[ -f /etc/squid3/squid.user.conf ]]; then
-  rm -rf /etc/squid3/squid.conf
-  ln -sf /etc/squid3/squid.user.conf /etc/squid3/squid.conf
-fi
+apply_backward_compatibility_fixes() {
+  if [[ -f /etc/squid3/squid.user.conf ]]; then
+    rm -rf /etc/squid3/squid.conf
+    ln -sf /etc/squid3/squid.user.conf /etc/squid3/squid.conf
+  fi
+}
+
+create_log_dir
+create_cache_dir
+apply_backward_compatibility_fixes
 
 # allow arguments to be passed to squid3
 if [[ ${1:0:1} = '-' ]]; then
